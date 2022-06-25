9ce82d361b7e8111f71a5bcf8e3b8f15facf0612
jvictor2k <jonvicej@gmail.com>
2022-06-25 02:53:54 -0300

    First commit.

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkinsSite.Models;
+
+namespace SkinsSite.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Skin> Skins { get; set; }
+    }
+}
