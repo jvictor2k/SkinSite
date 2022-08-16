0a89a6fdb9d6777a1510c452e95ff0edfb03e77e
jvictor2k <jonvicej@gmail.com>
2022-08-16 18:47:26 -0300

    Instalacao do nuget-pack Entity Framework Session, configuracao feita em startup e criacao da classe CarrinhoCompra.

--- Startup.cs
+++ Startup.cs
@@ -21,8 +21,13 @@ public class Startup
 
         services.AddTransient<ISkinRepository, SkinRepository>();
         services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
         services.AddControllersWithViews();
+
+        services.AddMemoryCache();
+        
+        services.AddSession();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +47,7 @@ public class Startup
         app.UseStaticFiles();
 
         app.UseRouting();
+        app.UseSession();
 
         app.UseAuthorization();
 
